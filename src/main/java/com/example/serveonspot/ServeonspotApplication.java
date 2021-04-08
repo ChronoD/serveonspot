154febd44a49b4d4862772bb1f5f7cd51efd9831
AndrejusN <andrejus.n@gmail.com>
2021-04-08 22:44:03 +0300

    first commit

---
+++ src/main/java/com/example/serveonspot/ServeonspotApplication.java
@@ -0,0 +1,13 @@
+package com.example.serveonspot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServeonspotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServeonspotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/serveonspot/ServeonspotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.serveonspot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServeonspotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
