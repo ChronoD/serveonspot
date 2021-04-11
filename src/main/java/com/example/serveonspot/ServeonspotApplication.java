824595e4175e128260d64f87061c14ae74bcac09
AndrejusN <andrejus.n@gmail.com>
2021-04-11 21:48:24 +0300

    defined interfaces

--- src/main/java/com/example/serveonspot/ServeonspotApplication.java
+++ src/main/java/com/example/serveonspot/ServeonspotApplication.java
@@ -1,13 +1,26 @@
 package com.example.serveonspot;
 
+import com.example.serveonspot.entities.Specialist;
+import com.example.serveonspot.repositories.SpecialistRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class ServeonspotApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ServeonspotApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ServeonspotApplication.class, args);
+    }
 
+
+    @Bean
+    public CommandLineRunner demo(SpecialistRepository specialistRepository) {
+        return (args) -> {
+            specialistRepository.save(new Specialist());
+            specialistRepository.save(new Specialist());
+            specialistRepository.save(new Specialist());
+        };
+    }
 }
