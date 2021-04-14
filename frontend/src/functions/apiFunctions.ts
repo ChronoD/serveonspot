b2bdb8f09970e4868015c48570e0ce369c9bc9f4
AndrejusN <andrejus.n@gmail.com>
2021-04-14 23:27:52 +0300

    basic mechanism

--- frontend/src/functions/apiFunctions.ts
+++ frontend/src/functions/apiFunctions.ts
@@ -3,7 +3,7 @@ import { Customer, Specialist } from "../UI";
 
 export function getSpecialistsHi(setData: (data: any) => void) {
   return axios
-    .get(`http://localhost:8080/api/specialists/hi`, {
+    .get(`http://localhost:8080/specialists/hi`, {
       headers: { "Access-Control-Allow-Origin": "*" },
     })
     .then((res) => {
@@ -14,11 +14,28 @@ export function getSpecialistsHi(setData: (data: any) => void) {
     });
 }
 
+export function initializeCustomersSource(
+  setCustomers: (customers: Customer[]) => void
+) {
+  const customersSource = new EventSource("http://127.0.0.1:8080/customers");
+  customersSource.onerror = () => {
+    if (customersSource.readyState === 2) {
+      setTimeout(initializeSpecialistsSource, 300);
+    }
+  };
+  customersSource.onmessage = (message) => {
+    console.log(message);
+
+    const data = JSON.parse(message.data);
+    setCustomers(data);
+  };
+}
+
 export function initializeSpecialistsSource(
   setSpecialists: (specialists: Specialist[]) => void
 ) {
   const specialistsSource = new EventSource(
-    "http://127.0.0.1:8080/api/specialists"
+    "http://127.0.0.1:8080/specialists"
   );
   specialistsSource.onerror = () => {
     if (specialistsSource.readyState === 2) {
@@ -30,24 +47,5 @@ export function initializeSpecialistsSource(
 
     const data = JSON.parse(message.data);
     setSpecialists(data);
-  };
-}
-
-export function initializeCustomersSource(
-  setCustomers: (customers: Customer[]) => void
-) {
-  const customersSource = new EventSource(
-    "http://127.0.0.1:8080/api/customers"
-  );
-  customersSource.onerror = () => {
-    if (customersSource.readyState === 2) {
-      setTimeout(initializeSpecialistsSource, 300);
-    }
-  };
-  customersSource.onmessage = (message) => {
-    console.log(message);
-
-    const data = JSON.parse(message.data);
-    setCustomers(data);
   };
 }
